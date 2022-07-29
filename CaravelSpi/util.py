8f82d14c91958eac1366be7f2c4cdd2a61965bba
devin-macy <dm426317@ohio.edu>
2022-07-29 13:13:16 -0400

    wip

--- CaravelSpi/util.py
+++ CaravelSpi/util.py
@@ -1,4 +1,4 @@
-import os
+import os, argparse
 from pathlib import Path
 from typing import Union
 
@@ -15,3 +15,18 @@ def from_bytes(data: bytes) -> int:
 
 def from_bin(path: Union[str, Path]) -> int:
     return os.path.getsize(path)
+
+def parse_input() -> None:
+    parser = argparse.ArgumentParser(description='Flash scipt for Caravel test harness', 
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f', '--file', action='store_false', help='Override supported file types')
+    override = parser.parse_args(['-f', '--file'])
+    print(override)
+    
+def get_input_type(input: str) -> str:
+    # file_type = input.partition('.')[2].lower()
+    # if file_type == 'hex' or file_type == 'bin':
+    #     return  file_type
+    # else:
+    #     raise RuntimeError('Invalid input file type. Needs to be .hex (verilog), .bin, or raw bytes')
+    pass
--- flash.py
+++ flash.py
@@ -10,6 +10,7 @@ mfg = int.from_bytes(hk.read_n(0x01, 2), 'big')
 product = hk.read_b(0x03)
 proj_id = int.from_bytes(hk.read_n(0x04, 4), 'big')
 
+print(f'Caravel Data:')
 print(f'  mfg    \t= {mfg:04x}')
 print(f'  product\t= {product:02x}')
 print(f'  proj ID\t= {proj_id:08x}\n')
@@ -20,19 +21,20 @@ flash.reset()
 print(f'status = 0x{flash.read_st1():02x}\n')
 
 jedec, mem_type, capacity = flash.read_jedec()
-print(f'JEDEC = 0x{jedec:02x}')
+print(f'JEDEC info:')
+print(f'manufacturer ID = 0x{jedec:02x}')
 print(f'memory type = 0x{mem_type:02x}')
-print(f'memory capacity = 0x{capacity:02x}')
+print(f'memory capacity = 0x{capacity:02x}\n')
 
-print('Erasing chip...')
+print('Erasing chip...\n')
 flasher.erase()
 
 print('Writing...')
 flasher.flash_hex(sys.argv[1])
-print(f'Total Bytes: {util.from_hex(sys.argv[1])}')
+print(f'Total Bytes: {util.from_hex(sys.argv[1])}\n')
 
 print('Verifying...')
 flasher.verify_hex(sys.argv[1])
-
-print('Done')
+    
+print('Done\n')
 print(f'status = 0x{flash.read_st1():02x}\n')
